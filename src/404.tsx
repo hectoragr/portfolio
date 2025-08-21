1e8100fe466abc275a7abcc1728dd71d693e9851
Hector Gomez <hector.agr@gmail.com>
2025-08-20 23:28:32 -0700

    Fix 404 image size and margins on small devices

--- src/404.tsx
+++ src/404.tsx
@@ -6,7 +6,7 @@ function FourOhFour() {
     const { t } = useTranslation();
     return (
         <div className="four-oh-four centered-content">
-            <p><img src="img/chatgpt-404-octopus.png" alt="Page not found" style={{ width: '480px', height: 'auto', textAlign: 'center' }} /></p>
+            <p><img src="img/chatgpt-404-octopus.png" alt="Page not found" /></p>
             <p style={{ textAlign: 'center' }}>{t('404.p1', 'The page you are looking for does not exist.')}</p>
             <p style={{ textAlign: 'center' }}>{t('404.p2','Please check the URL or return to')} <a href="/">{t('home.title', 'Home')}</a>.</p>
         </div>
