acabd034b65158b57720e76583d5baec13f280cc
Hector Gomez <hector.agr@gmail.com>
2026-03-23 00:53:47 -0700

    Migrate from react-scripts to Vite 5

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null
+        return transformWithEsbuild(code, id, { loader: 'jsx' })
+      },
+    },
+    react(),
+  ],
+  build: {
+    outDir: 'build',
+  },
+  publicDir: 'public',
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+})
